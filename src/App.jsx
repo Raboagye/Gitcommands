4d3f16709de3ff4eff6f764df49cf18ddae9cda2
Raboagye2 <richyaboagye@gmail.com>
2023-10-23 17:15:44 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import axios from 'axios'
+import './App.css'
+import { useEffect, useState } from 'react'
+
+function App() {
+
+  const [statee, setStatee] = useState("")
+
+  useEffect(() => {
+    axios.get("https://api.github.com/users/hacktivist123").then((response) => {
+      setStatee(response.data);
+    });
+  }, []);
+  
+
+  return (
+    <>   
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+        <div>{JSON.stringify(statee, null, 2)}</div>
+      </p>
+    </>
+  )
+}
+
+export default App
